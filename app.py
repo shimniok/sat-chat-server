d68675b909d67e9d14e22debfe374023aa364e09
shimniok@gmail.com <shimniok@gmail.com>
2020-03-13 17:49:41 -0600

    added index.html template, css file, modified config to fix secret key

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
 from flask import Flask
+from flask import render_template
+import config
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+
+@app.route('/send', methods=['post'])
+def send():
+    return "Send!"
 
 
 if __name__ == '__main__':
     app.run()
-
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
+    print(os.environ['IMEI'])
     SECRET_KEY = os.environ['SECRET_KEY']
     IMEI = os.environ['IMEI']
     USERNAME = os.environ['USERNAME']
