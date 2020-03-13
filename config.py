a5a40bb1ecdc399412211c8e26129c03de983152
shimniok@gmail.com <shimniok@gmail.com>
2020-03-13 17:14:13 -0600

    Added config file

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ['SECRET_KEY']
+    IMEI = os.environ['IMEI']
+    USERNAME = os.environ['USERNAME']
+    PASSWORD = os.environ['PASSWORD']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
