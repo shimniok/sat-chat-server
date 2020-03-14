f89654bcc0119a8fd286cb478bdc6712fdd48beb
shimniok@gmail.com <shimniok@gmail.com>
2020-03-13 19:04:40 -0600

    implemented send to rock7 api, fixed issues with encoding, configuration

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask, render_template, request
 import config
 import requests
+import binascii
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
 
 @app.route('/')
 def hello():
@@ -15,16 +15,19 @@ def hello():
 @app.route('/api/send', methods=['post'])
 def send():
     text = request.form.get('message')
+    hex = binascii.b2a_hex(text.encode('utf-8'))
+
+    print(hex)
 
     data = {'username': app.config['USERNAME'],
         'password': app.config['PASSWORD'],
         'imei': app.config['IMEI'],
-        'data': text.encode('hex')
+        'data': hex
     }
 
-    r = requests.post(url = API_ENDPOINT, data = data)
+    r = requests.post(url = app.config['API_ENDPOINT'], data = data )
 
-    return r
+    return render_template("send.html", r=r.text)
 
 
 if __name__ == '__main__':
