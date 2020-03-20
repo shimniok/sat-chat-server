4d91118d269cd14826f2fa515cc4be4e1f7cd331
shimniok@gmail.com <shimniok@gmail.com>
2020-03-20 13:55:29 -0600

    Implemented database save on receive route; get all on message route

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
 import os
 import config
 import requests
 import binascii
+from flask import Flask, render_template, request, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Message
+from models import *
 
 
 #TODO implement authentication
@@ -22,7 +22,7 @@ def hello():
     return render_template("index.html")
 
 # Send data to Rock7
-@app.route('/api/send', methods=['post'])
+@app.route('/api/send', methods=['POST'])
 def send():
     text = request.form.get('message')
     hex = binascii.b2a_hex(text.encode('utf-8'))
@@ -41,8 +41,10 @@ def send():
     return render_template("send.html", r=r.text)
 
 # Receive data from Rock7
-@app.route('/api/receive', methods=['post'])
+@app.route('/api/receive', methods=['POST'])
 def receive():
+#    status_code = status.HTTP_400_BAD_REQUEST
+#    error_msg = { 'error': 'generic error' }
     imei = request.form.get('imei')
     if (imei == app.config['IMEI']):
         momsn = request.form.get('momsn')
@@ -53,16 +55,29 @@ def receive():
         hex = request.form.get('data')
         text = binascii.a2b_hex(hex).decode("utf-8")
 
-        #try:
-        #    result = Message(message=text)
-        #    db.session.add(result)
-        #    db.session.commit()
-        #except:
-        #    errors.append("Unable to add item to database.")
+        # Add message to database
+        try:
+            msg = Message(message = text)
+            db.session.add(msg)
+            db.session.commit()
+            status_code = status.HTTP_200_OK
+        except:
+            #TODO: secure error message & logging
+            error_msg = { 'error': 'unable to add to database' }
+    else:
+        error_msg = { 'error': 'imei mismatch' }
+        status_code = status.HTTP_400_BAD_REQUEST
 
-        #TODO parse return from request and return json
+    return "done"
 
-        return "done"
+@app.route('/api/message', methods=['GET'])
+def messages():
+    list = []
+    for i, m in db.session.query(Message.id, Message.message):
+        list.append({'id': i, 'message': m})
+    return jsonify(list)
+    #print(">>> ", list)
+    #return "ok" #message_list
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
+import json
 
 class Message(db.Model):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String())
+
     #from_user = db.Column(db.String())
     #to_user = db.Column(db.String())
-    message = db.Column(db.String())
     #json = db.Column(JSON)
     #momsn = request.form.get('momsn')
     #transmit_time = request.form.get('transmit_time')
@@ -19,21 +21,23 @@ class Message(db.Model):
     def __init__(self, message):
         self.message = message
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+    #def __repr__(self):
+    #    return "Message(<id='{}', message='{}'>)".format(self.id, self.message)
+        #data = { "id": self.id, "message": self.message }
+        #return data
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-    email = db.Column(db.String)
-
-    def __init__(self, username, password, email):
-        self.username = username
-        self.password = password
-        self.email = email
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+#class User(db.Model):
+#    __tablename__ = 'users'
+#
+#    id = db.Column(db.Integer, primary_key=True)
+#    username = db.Column(db.String)
+#    password = db.Column(db.String)
+#    email = db.Column(db.String)
+#
+#    def __init__(self, username, password, email):
+#        self.username = username
+#        self.password = password
+#        self.email = email
+#
+#    def __repr__(self):
+#        return '<id {}>'.format(self.id)
