b9e03a75060db604644fb8b6656d84ef5ec83be8
shimniok@gmail.com <shimniok@gmail.com>
2020-07-08 10:49:41 -0600

    removed useless length params from models

--- models.py
+++ models.py
@@ -12,11 +12,11 @@ class Message(OutputMixin, db.Model):
     # TODO: add IMEI column, to/from
     id = db.Column(db.Integer, primary_key=True)
     momsn = db.Column(db.Integer)
-    message = db.Column(db.String(150))
+    message = db.Column(db.String())
     transmit_time = db.Column(db.DateTime())
     time = db.Column(db.DateTime())
-    iridium_latitude = db.Column(db.Float(12))
-    iridium_longitude = db.Column(db.Float(12))
+    iridium_latitude = db.Column(db.Float())
+    iridium_longitude = db.Column(db.Float())
     iridium_cep = db.Column(db.Integer)
 
     def __repr__(self):
@@ -27,9 +27,9 @@ class User(OutputMixin, UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(320), unique=True)
-    name = db.Column(db.String(32))
-    password = db.Column(db.String(100))
+    email = db.Column(db.String(), unique=True)
+    name = db.Column(db.String())
+    password = db.Column(db.String())
 
     def __repr__(self):
         return "User(<id='{}', name='{}', password='{}', email='{}'>".format(
