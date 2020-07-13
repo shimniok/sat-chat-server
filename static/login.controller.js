ce913d720ee471dedc08b148655f2f4505b22125
shimniok@gmail.com <shimniok@gmail.com>
2020-07-13 15:15:40 -0600

    Implemented login controller, revised login template

--- static/login.controller.js
+++ static/login.controller.js
@@ -1,6 +1,34 @@
 angular.module('login', [])
-.controller('LoginController', [
-  function() {
+
+.service('SessionService', function() {
+  var user = null;
+
+  this.isAuthenticated = function(value){
+    return user != null;
+  };
+
+})
+
+.controller('LoginController', [ '$scope', '$log', '$http',
+  function($scope, $log, $http) {
+
+    $scope.authenticate = function() {
+      $log.log("authenticate()");
+
+      var data = {
+        "email": $scope.email,
+        "password": $scope.password
+      };
+      var config = { headers: { 'Content-Type': 'application/json', 'Accepts': 'application/json' } }
+      $http.post('/auth', data, config)
+        .success(function(result) {
+          $log.log("success");
+          this.user = result;
+        },
+        function() {
+          $log.log("failure");
+        });
+      };
 
   }
 ]);
