cbd8e812e52d30d86d66e4f13b1a65d62c849d37
shimniok@gmail.com <shimniok@gmail.com>
2020-07-15 09:59:37 -0600

    Split session service into separate file

--- static/login.controller.js
+++ static/login.controller.js
@@ -1,29 +1,5 @@
 angular.module('login', ['auth'])
 
-.service('SessionService', [ '$log', 'AuthProvider',
-function($log, auth) {
-  var user = auth.get();
-
-  this.valid = function() {
-    return user.$promise;
-  };
-
-  this.login = function(username, password, success, failure) {
-    auth.save({ "email": username, "password": password }).$promise
-    .then(function(result) {
-      user = result;
-      success(result);
-    },function(result) {
-      failure(result);
-    });
-  }
-
-  this.logout = function() {
-    user = auth.delete();
-  }
-
-}])
-
 .controller('SessionController', [ '$scope', '$log', '$location', 'SessionService',
   function ($scope, $log, $location, session) {
     $scope.$on('$routeChangeStart', function (angularEvent, next, current) {
---
+++ static/login.service.js
@@ -0,0 +1,25 @@
+angular.module('login')
+
+.service('SessionService', [ '$log', 'AuthProvider',
+function($log, auth) {
+  var user = auth.get();
+
+  this.valid = function() {
+    return user.$promise;
+  };
+
+  this.login = function(username, password, success, failure) {
+    auth.save({ "email": username, "password": password }).$promise
+    .then(function(result) {
+      user = result;
+      success(result);
+    },function(result) {
+      failure(result);
+    });
+  }
+
+  this.logout = function() {
+    user = auth.delete();
+  }
+
+}])
