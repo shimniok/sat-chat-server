042d82a4054f2afc7cb4633f4dbdd4fee981839b
shimniok@gmail.com <shimniok@gmail.com>
2020-07-31 11:40:52 -0600

    added endpoint var. implemented authN. Fixes to post.

--- device.py
+++ device.py
@@ -6,44 +6,47 @@ from flask_sqlalchemy import SQLAlchemy
 from models import Device
 from models import db
 
-device_bp = Blueprint('device', __name__, url_prefix='/api')
+endpoint = '/api/device'
+
+device_bp = Blueprint('device', __name__)
 
 # TODO: admin role can interact with anything
 
 @device_bp.before_request
 def device_before():
     print("device_before()")
-    #if not current_user.is_authenticated:
-    #    abort(401)
+    if not current_user.is_authenticated:
+        abort(401)
 
 
-@device_bp.route('/device', methods=['get'])
+@device_bp.route(endpoint, methods=['get'])
 def device_get():
     devices = Device.query.all()
 
     return jsonify([d.to_dict() for d in devices])
 
 
-@device_bp.route('/device', methods=['post'])
+@device_bp.route(endpoint, methods=['post'])
 def devices_post():
     if not request.json:
         abort(400)
     try:
-        dev = Device()
-        dev.imei = request.json['imei']
-        dev.username = request.json['username']
-        dev.password = request.json['password']
+        dev = Device(
+            imei = request.json['imei'],
+            username = request.json['username'],
+            password = request.json['password']
+        )
+        db.session.add(dev)
+        db.session.commit()
 
     except Exception as e:
         return "Bad request: {}".format(e), 400
 
-    db.session.add(dev)
-    db.session.commit()
 
     return jsonify(dev.to_dict())
 
 
-@device_bp.route('/device/<id>', methods=['put'])
+@device_bp.route(endpoint+'/<id>', methods=['put'])
 def device_put(id):
     if not request.json:
         abort(400)
@@ -60,7 +63,7 @@ def device_put(id):
     return jsonify(dev.to_dict())
 
 
-@device_bp.route('/device/<id>', methods=['delete'])
+@device_bp.route(endpoint+'/<id>', methods=['delete'])
 def device_del(id):
     try:
         dev = Device.query.filter_by(id = id).first_or_404()
