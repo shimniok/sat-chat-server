e642b7c8d86f0a67661f03d207a64933daa46ffc
shimniok@gmail.com <shimniok@gmail.com>
2020-07-31 14:31:27 -0600

    Auto fill form, removed duplicate controller

--- static/device.controller.js
+++ static/device.controller.js
@@ -4,6 +4,18 @@ angular.module('device', ['ngResource'])
   function($scope, $log, DeviceService) {
     $scope.myDevice = {};
 
+    // see if we have a device already and if so, assign to model
+    DeviceService.query(content_type='application/json').$promise
+    .then(function(result) {
+      $log.log(result);
+      if (result.length != 0) {
+        r = result[0];
+        $scope.username = r.username;
+        $scope.imei = r.imei;
+        $scope.password = "password"; // bogus password
+      }
+    });
+
     $log.log('device controller');
 
     $scope.save = function() {
