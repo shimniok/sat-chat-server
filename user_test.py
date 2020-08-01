c4a0d8a94e51f6fcc9880050d050d4449f1b26f2
shimniok@gmail.com <shimniok@gmail.com>
2020-08-01 17:14:59 -0600

    Added error messages

--- user_test.py
+++ user_test.py
@@ -12,7 +12,7 @@ def test_user_list(client):
     """Start with fresh database with initial user."""
 
     r = client.get(endpoint, content_type="application/json")
-    assert r.status_code == 200
+    assert r.status_code == 200, 'Error {}'.format(r.data)
 
     # only one user returned
     d = r.json #json.loads(r.data.decode())
@@ -30,12 +30,12 @@ def test_user_list(client):
 
 def test_user_post(client):
     r = client.post(endpoint, json=new, content_type="application/json")
-    assert r.status_code == 200
+    assert r.status_code == 200, 'Error {}'.format(r.data)
     assert r.content_type == 'application/json'
     u = r.json
     assert 'id' in u
     r = client.get('/api/user/{}'.format(u['id']))
-    assert r.status_code == 200
+    assert r.status_code == 200, 'Error {}'.format(r.data)
     assert r.content_type == 'application/json'
     u = r.json
     assert u['name'] == new['name']
