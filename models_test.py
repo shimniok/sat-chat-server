0dd102353237d234f439aaf5def9bc53952b2226
shimniok@gmail.com <shimniok@gmail.com>
2020-08-01 17:04:34 -0600

    cleanup models test

--- models_test.py
+++ models_test.py
@@ -14,19 +14,6 @@ def test_db_message(application):
     messages = Message.query.all()
     assert len(messages) == 0, 'table not empty'
 
-    m = Message(
-        imei='abcdefg',
-        sender_id=1,
-        momsn=99,
-        message='testing',
-        transmit_time="2020-08-01T08:33Z",
-        time="2021-09-02T11:22Z",
-        iridium_latitude=-104.1234,
-        iridium_longitude=39.4321,
-        iridium_cep=4
-    )
-    #TODO: test variants of Message init
-    assert m.imei == 'abcdefg'
 
 def test_db_device(application):
     ''' Device table should be empty '''
