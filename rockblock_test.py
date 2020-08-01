751a9613363d2dd9c942a276344869948912f9ed
shimniok@gmail.com <shimniok@gmail.com>
2020-08-01 12:15:47 -0600

    added test for receive

--- rockblock_test.py
+++ rockblock_test.py
@@ -1,10 +1,40 @@
 from test_fixture import *
+from datetime import datetime
+import binascii
+import message
 
-send_data = {
+mo_msg = {
+    'imei': os.environ['IMEI'],
+    'momsn': 99,
+    'transmit_time': datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%SZ"),
+    'iridium_latitude': 39.5807,
+    'iridium_longitude': -104.8772,
+    'iridium_cep': 8,
+    'data': binascii.b2a_hex('Test Message'.encode('utf-8'))
+}
+
+def test_receive(client):
+    r = client.post('/api/receive', data=mo_msg)
+    assert r.status_code == 200, 'Error {}'.format(r.data)
+    assert r.data == b'done'
+    # Check database
+    r = client.get(message.endpoint, content_type="application/json")
+    assert r.status_code == 200, 'Error {}'.format(r.data)
+    assert len(r.json) == 1
+    m = r.json[0]
+    #assert m['imei'] == mo_msg['imei']
+    assert m['momsn'] == mo_msg['momsn']
+    assert m['transmit_time'] == mo_msg['transmit_time']
+    assert m['iridium_latitude'] == mo_msg['iridium_latitude']
+    assert m['iridium_longitude'] == mo_msg['iridium_longitude']
+    assert m['iridium_cep'] == mo_msg['iridium_cep']
+    #assert m['data'] == mo_msg['data']
+
+mt_msg = {
     "message": "This is a test"
 }
 
 def test_send(client):
-    r = client.post('/api/send', json=send_data, content_type="application/json")
+    r = client.post('/api/send', json=mt_msg, content_type="application/json")
     assert r.status_code == 200, 'Error {}'.format(r.data)
     assert r.content_type == 'application/json'
