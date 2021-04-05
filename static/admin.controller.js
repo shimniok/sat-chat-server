33940852bcbb0188a0746872600e9add1ab538aa
Michael Shimniok <shimniok@gmail.com>
2021-04-05 09:41:42 -0600

    fixed message delete on admin page

--- static/admin.controller.js
+++ static/admin.controller.js
@@ -1,13 +1,14 @@
 angular.module('admin', ['message'])
 
 .controller('AdminController', ['$scope', '$log', 'MessageService',
-function($scope, $log, message) {
+function($scope, $log, messageService) {
 
-  $scope.messages = message.query();
+  $scope.messages = messageService.query();
 
   $scope.deleteMessage = function(m) {
     $log.log("deleteMessage() " + m.id);
-    m.$delete().then(function() {
+    messageService.delete(m).$promise
+    .then(function() {
       var index = $scope.messages.indexOf(m);
       $scope.messages.splice(index, 1);
     });
