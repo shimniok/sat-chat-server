d206bf9339a572463fa8b911aa7fb0e47449b76c
Michael Shimniok <shimniok@gmail.com>
2021-04-19 09:39:58 -0600

    Database migration

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from config import create_config
-from models import db
+from api.models import db
 from app import create_app
 
 app = create_app()
